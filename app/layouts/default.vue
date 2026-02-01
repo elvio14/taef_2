14522dcfe34fdcd512f3f0a70289128b11fb21b8
Andrew <andrew.elvio14@gmail.com>
2026-02-01 02:01:43 -0500

    Header env safe area

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -244,7 +244,7 @@ onUnmounted(() => {
     }
 
     #slider-container.mobile {
-        top: 1.5rem;
+        top: calc(env(safe-area-inset-top, 0px) + 1.5rem);
         left: 70%;
         right: auto;
         width: 260px;
